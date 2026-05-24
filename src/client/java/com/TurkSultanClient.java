c2bea9f8e7cbe34cc6df4d1e4d8128dc47d8abc8
benimgmailimaa-beep <benimgmailimaa@gmail.com>
2026-05-24 22:21:20 +0300

    Update TurkSultanClient.java

--- src/client/java/com/TurkSultanClient.java
+++ src/client/java/com/TurkSultanClient.java
@@ -1,1 +1,31 @@
+package com;
 
+import com.example.gui.ModulesScreen;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class TurkSultanClient implements ClientModInitializer {
+    private static KeyBinding menuKey;
+
+    @Override
+    public void onInitializeClient() {
+        menuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.turksultan.menu",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_RIGHT_SHIFT,
+                "category.turksultan"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (menuKey.wasPressed()) {
+                if (client.currentScreen == null) {
+                    client.setScreen(new ModulesScreen());
+                }
+            }
+        });
+    }
+}
