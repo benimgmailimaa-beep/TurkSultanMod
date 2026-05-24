7e6900263c49cbdccb22f7d59c944894ed778ffa
benimgmailimaa-beep <benimgmailimaa@gmail.com>
2026-05-24 19:55:50 +0300

    Update and rename src/main/java/com/example/ExampleMod.java to src/main/java/com/TurkSultanClient.java

---
+++ src/main/java/com/TurkSultanClient.java
@@ -0,0 +1,10 @@
+package com.example.examplemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TurkSultanClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("TurkSultan Modu Başlatıldı!");
+    }
+}
